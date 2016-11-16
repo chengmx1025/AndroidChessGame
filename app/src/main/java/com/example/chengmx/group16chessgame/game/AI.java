a1bffbc0c503df8995237c783dc9dd3d0648fe02
chengmx1025 <569464243@qq.com>
2016-11-16 13:57:41 +0800

    ver3.5.1 Small bugs fixed

--- app/src/main/java/com/example/chengmx/group16chessgame/game/AI.java
+++ app/src/main/java/com/example/chengmx/group16chessgame/game/AI.java
@@ -11,6 +11,9 @@ import java.util.Random;
  */
 
 public class AI {
+    private static final byte BLACK = -1;
+    private static final byte WHITE = 1;
+
     private int[][] valueTable = {
     {0,0,0,0,0,0,0,0,0,0},
     {0,1,1,1,1,1,1,1,1,0},
@@ -34,127 +37,127 @@ public class AI {
                 }
                 else{
                     //1
-                    if(i>=1 && chessBoard[i-1][j]==-1)
+                    if(i>=1 && chessBoard[i-1][j]==WHITE)
                         valueTable[i][j] += 10;
-                    if(i>=1 && chessBoard[i-1][j]==1)
+                    if(i>=1 && chessBoard[i-1][j]==BLACK)
                         valueTable[i][j] += 8;
-                    if(j>=1 && chessBoard[i][j-1]==-1)
+                    if(j>=1 && chessBoard[i][j-1]==WHITE)
                         valueTable[i][j] += 10;
-                    if(j>=1 && chessBoard[i][j-1]==1)
+                    if(j>=1 && chessBoard[i][j-1]==BLACK)
                         valueTable[i][j] += 8;
-                    if(i<=8 && chessBoard[i+1][j]==-1)
+                    if(i<=8 && chessBoard[i+1][j]==WHITE)
                         valueTable[i][j] += 10;
-                    if(i<=8 && chessBoard[i+1][j]==1)
+                    if(i<=8 && chessBoard[i+1][j]==BLACK)
                         valueTable[i][j] += 8;
-                    if(j<=8 && chessBoard[i][j+1]==-1)
+                    if(j<=8 && chessBoard[i][j+1]==WHITE)
                         valueTable[i][j] += 10;
-                    if(j<=8 && chessBoard[i][j+1]==1)
+                    if(j<=8 && chessBoard[i][j+1]==BLACK)
                         valueTable[i][j] += 8;
 
                     //2
-                    if(i>=2 && chessBoard[i-1][j]==-1 && chessBoard[i-2][j]==-1)
+                    if(i>=2 && chessBoard[i-1][j]==WHITE && chessBoard[i-2][j]==WHITE)
                         valueTable[i][j] += 300;
-                    if(i>=2 && chessBoard[i-1][j]==1 && chessBoard[i-2][j]==1)
+                    if(i>=2 && chessBoard[i-1][j]==BLACK && chessBoard[i-2][j]==BLACK)
                         valueTable[i][j] += 200;
-                    if(j>=2 && chessBoard[i][j-1]==-1 && chessBoard[i][j-2]==-1)
+                    if(j>=2 && chessBoard[i][j-1]==WHITE && chessBoard[i][j-2]==WHITE)
                         valueTable[i][j] += 300;
-                    if(j>=2 && chessBoard[i][j-1]==1 && chessBoard[i][j-2]==1)
+                    if(j>=2 && chessBoard[i][j-1]==BLACK && chessBoard[i][j-2]==BLACK)
                         valueTable[i][j] += 200;
-                    if(i<=7 && chessBoard[i+1][j]==-1 && chessBoard[i+2][j]==-1)
+                    if(i<=7 && chessBoard[i+1][j]==WHITE && chessBoard[i+2][j]==WHITE)
                         valueTable[i][j] += 300;
-                    if(i<=7 && chessBoard[i+1][j]==1 && chessBoard[i+2][j]==1)
+                    if(i<=7 && chessBoard[i+1][j]==BLACK && chessBoard[i+2][j]==BLACK)
                         valueTable[i][j] += 200;
-                    if(j<=7 && chessBoard[i][j+1]==-1 && chessBoard[i][j+2]==-1)
+                    if(j<=7 && chessBoard[i][j+1]==WHITE && chessBoard[i][j+2]==WHITE)
                         valueTable[i][j] += 300;
-                    if(j<=7 && chessBoard[i][j+1]==1 && chessBoard[i][j+2]==1)
+                    if(j<=7 && chessBoard[i][j+1]==BLACK && chessBoard[i][j+2]==BLACK)
                         valueTable[i][j] += 200;
 
-                    if(i>=2 && j>=2 && chessBoard[i-1][j-1]==-1 && chessBoard[i-2][j-2]==-1)
+                    if(i>=2 && j>=2 && chessBoard[i-1][j-1]==WHITE && chessBoard[i-2][j-2]==WHITE)
                         valueTable[i][j] += 300;
-                    if(i>=2 && j>=2 && chessBoard[i-1][j-1]==1 && chessBoard[i-2][j-2]==1)
+                    if(i>=2 && j>=2 && chessBoard[i-1][j-1]==BLACK && chessBoard[i-2][j-2]==BLACK)
                         valueTable[i][j] += 200;
-                    if(i>=2 && j<=7 && chessBoard[i-1][j+1]==-1 && chessBoard[i-2][j+2]==-1)
+                    if(i>=2 && j<=7 && chessBoard[i-1][j+1]==WHITE && chessBoard[i-2][j+2]==WHITE)
                         valueTable[i][j] += 300;
-                    if(i>=2 && j<=7 && chessBoard[i-1][j+1]==1 && chessBoard[i-2][j+2]==1)
+                    if(i>=2 && j<=7 && chessBoard[i-1][j+1]==BLACK && chessBoard[i-2][j+2]==BLACK)
                         valueTable[i][j] += 200;
-                    if(i<=7 && j>=2 && chessBoard[i+1][j-1]==-1 && chessBoard[i+2][j-2]==-1)
+                    if(i<=7 && j>=2 && chessBoard[i+1][j-1]==WHITE && chessBoard[i+2][j-2]==WHITE)
                         valueTable[i][j] += 300;
-                    if(i<=7 && j>=2 && chessBoard[i+1][j-1]==1 && chessBoard[i+2][j-2]==1)
+                    if(i<=7 && j>=2 && chessBoard[i+1][j-1]==BLACK && chessBoard[i+2][j-2]==BLACK)
                         valueTable[i][j] += 200;
-                    if(i<=7 && j<=7 && chessBoard[i+1][j+1]==-1 && chessBoard[i+2][j+2]==-1)
+                    if(i<=7 && j<=7 && chessBoard[i+1][j+1]==WHITE && chessBoard[i+2][j+2]==WHITE)
                         valueTable[i][j] += 300;
-                    if(i<=7 && j<=7 && chessBoard[i+1][j+1]==1 && chessBoard[i+2][j+2]==1)
+                    if(i<=7 && j<=7 && chessBoard[i+1][j+1]==BLACK && chessBoard[i+2][j+2]==BLACK)
                         valueTable[i][j] += 200;
 
                     //3
-                    if(i>=3 && chessBoard[i-1][j]==-1 && chessBoard[i-2][j]==-1 && chessBoard[i-3][j]==-1)
+                    if(i>=3 && chessBoard[i-1][j]==WHITE && chessBoard[i-2][j]==WHITE && chessBoard[i-3][j]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(i>=3 && chessBoard[i-1][j]==1 && chessBoard[i-2][j]==1 && chessBoard[i-3][j]==1)
+                    if(i>=3 && chessBoard[i-1][j]==BLACK && chessBoard[i-2][j]==BLACK && chessBoard[i-3][j]==BLACK)
                         valueTable[i][j] += 2000;
-                    if(j>=3 && chessBoard[i][j-1]==-1 && chessBoard[i][j-2]==-1 && chessBoard[i][j-3]==-1)
+                    if(j>=3 && chessBoard[i][j-1]==WHITE && chessBoard[i][j-2]==WHITE && chessBoard[i][j-3]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(j>=3 && chessBoard[i][j-1]==1 && chessBoard[i][j-2]==1 && chessBoard[i][j-3]==1)
+                    if(j>=3 && chessBoard[i][j-1]==BLACK && chessBoard[i][j-2]==BLACK && chessBoard[i][j-3]==BLACK)
                         valueTable[i][j] += 2000;
-                    if(i<=6 && chessBoard[i+1][j]==-1 && chessBoard[i+2][j]==-1 && chessBoard[i+3][j]==-1)
+                    if(i<=6 && chessBoard[i+1][j]==WHITE && chessBoard[i+2][j]==WHITE && chessBoard[i+3][j]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(i<=6 && chessBoard[i+1][j]==1 && chessBoard[i+2][j]==1 && chessBoard[i+3][j]==1)
+                    if(i<=6 && chessBoard[i+1][j]==BLACK && chessBoard[i+2][j]==BLACK && chessBoard[i+3][j]==BLACK)
                         valueTable[i][j] += 2000;
-                    if(j<=6 && chessBoard[i][j+1]==-1 && chessBoard[i][j+2]==-1 && chessBoard[i][j+3]==-1)
+                    if(j<=6 && chessBoard[i][j+1]==WHITE && chessBoard[i][j+2]==WHITE && chessBoard[i][j+3]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(j<=6 && chessBoard[i][j+1]==1 && chessBoard[i][j+2]==1 && chessBoard[i][j+3]==1)
+                    if(j<=6 && chessBoard[i][j+1]==BLACK && chessBoard[i][j+2]==BLACK && chessBoard[i][j+3]==BLACK)
                         valueTable[i][j] += 2000;
 
-                    if(i>=3 && j>=3 && chessBoard[i-1][j-1]==-1 && chessBoard[i-2][j-2]==-1 && chessBoard[i-3][j-3]==-1)
+                    if(i>=3 && j>=3 && chessBoard[i-1][j-1]==WHITE && chessBoard[i-2][j-2]==WHITE && chessBoard[i-3][j-3]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(i>=3 && j>=3 && chessBoard[i-1][j-1]==1 && chessBoard[i-2][j-2]==1 && chessBoard[i-3][j-3]==1)
+                    if(i>=3 && j>=3 && chessBoard[i-1][j-1]==BLACK && chessBoard[i-2][j-2]==BLACK && chessBoard[i-3][j-3]==BLACK)
                         valueTable[i][j] += 2000;
-                    if(i>=3 && j<=6 && chessBoard[i-1][j+1]==-1 && chessBoard[i-2][j+2]==-1 && chessBoard[i-3][j+3]==-1)
+                    if(i>=3 && j<=6 && chessBoard[i-1][j+1]==WHITE && chessBoard[i-2][j+2]==WHITE && chessBoard[i-3][j+3]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(i>=3 && j<=6 && chessBoard[i-1][j+1]==1 && chessBoard[i-2][j+2]==1 && chessBoard[i-3][j+3]==1)
+                    if(i>=3 && j<=6 && chessBoard[i-1][j+1]==BLACK && chessBoard[i-2][j+2]==BLACK && chessBoard[i-3][j+3]==BLACK)
                         valueTable[i][j] += 2000;
-                    if(i<=6 && j>=3 && chessBoard[i+1][j-1]==-1 && chessBoard[i+2][j-2]==-1 && chessBoard[i+3][j-3]==-1)
+                    if(i<=6 && j>=3 && chessBoard[i+1][j-1]==WHITE && chessBoard[i+2][j-2]==WHITE && chessBoard[i+3][j-3]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(i<=6 && j>=3 && chessBoard[i+1][j-1]==1 && chessBoard[i+2][j-2]==1 && chessBoard[i+3][j-3]==1)
+                    if(i<=6 && j>=3 && chessBoard[i+1][j-1]==BLACK && chessBoard[i+2][j-2]==BLACK && chessBoard[i+3][j-3]==BLACK)
                         valueTable[i][j] += 2000;
-                    if(i<=6 && j<=6 && chessBoard[i+1][j+1]==-1 && chessBoard[i+2][j+2]==-1 && chessBoard[i+3][j+3]==-1)
+                    if(i<=6 && j<=6 && chessBoard[i+1][j+1]==WHITE && chessBoard[i+2][j+2]==WHITE && chessBoard[i+3][j+3]==WHITE)
                         valueTable[i][j] += 4000;
-                    if(i<=6 && j<=6 && chessBoard[i+1][j+1]==1 && chessBoard[i+2][j+2]==1 && chessBoard[i+3][j+3]==1)
+                    if(i<=6 && j<=6 && chessBoard[i+1][j+1]==BLACK && chessBoard[i+2][j+2]==BLACK && chessBoard[i+3][j+3]==BLACK)
                         valueTable[i][j] += 2000;
 
 
                     //4
-                    if(i>=4 && chessBoard[i-1][j]==-1 && chessBoard[i-2][j]==-1 && chessBoard[i-3][j]==-1 && chessBoard[i-4][j]==-1)
+                    if(i>=4 && chessBoard[i-1][j]==WHITE && chessBoard[i-2][j]==WHITE && chessBoard[i-3][j]==WHITE && chessBoard[i-4][j]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(i>=4 && chessBoard[i-1][j]==1 && chessBoard[i-2][j]==1 && chessBoard[i-3][j]==1 && chessBoard[i-4][j]==1)
+                    if(i>=4 && chessBoard[i-1][j]==BLACK && chessBoard[i-2][j]==BLACK && chessBoard[i-3][j]==BLACK && chessBoard[i-4][j]==BLACK)
                         valueTable[i][j] += 20000;
-                    if(j>=4 && chessBoard[i][j-1]==-1 && chessBoard[i][j-2]==-1 && chessBoard[i][j-3]==-1 && chessBoard[i][j-4]==-1)
+                    if(j>=4 && chessBoard[i][j-1]==WHITE && chessBoard[i][j-2]==WHITE && chessBoard[i][j-3]==WHITE && chessBoard[i][j-4]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(j>=4 && chessBoard[i][j-1]==1 && chessBoard[i][j-2]==1 && chessBoard[i][j-3]==1 && chessBoard[i][j-4]==1)
+                    if(j>=4 && chessBoard[i][j-1]==BLACK && chessBoard[i][j-2]==BLACK && chessBoard[i][j-3]==BLACK && chessBoard[i][j-4]==BLACK)
                         valueTable[i][j] += 20000;
-                    if(i<=5 && chessBoard[i+1][j]==-1 && chessBoard[i+2][j]==-1 && chessBoard[i+3][j]==-1 && chessBoard[i+4][j]==-1)
+                    if(i<=5 && chessBoard[i+1][j]==WHITE && chessBoard[i+2][j]==WHITE && chessBoard[i+3][j]==WHITE && chessBoard[i+4][j]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(i<=5 && chessBoard[i+1][j]==1 && chessBoard[i+2][j]==1 && chessBoard[i+3][j]==1 && chessBoard[i+4][j]==1)
+                    if(i<=5 && chessBoard[i+1][j]==BLACK && chessBoard[i+2][j]==BLACK && chessBoard[i+3][j]==BLACK && chessBoard[i+4][j]==BLACK)
                         valueTable[i][j] += 20000;
-                    if(j<=5 && chessBoard[i][j+1]==-1 && chessBoard[i][j+2]==-1 && chessBoard[i][j+3]==-1 && chessBoard[i][j+4]==-1)
+                    if(j<=5 && chessBoard[i][j+1]==WHITE && chessBoard[i][j+2]==WHITE && chessBoard[i][j+3]==WHITE && chessBoard[i][j+4]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(j<=5 && chessBoard[i][j+1]==1 && chessBoard[i][j+2]==1 && chessBoard[i][j+3]==1 && chessBoard[i][j+4]==1)
+                    if(j<=5 && chessBoard[i][j+1]==BLACK && chessBoard[i][j+2]==BLACK && chessBoard[i][j+3]==BLACK && chessBoard[i][j+4]==BLACK)
                         valueTable[i][j] += 20000;
 
-                    if(i>=4 && j>=4 && chessBoard[i-1][j-1]==-1 && chessBoard[i-2][j-2]==-1 && chessBoard[i-3][j-3]==-1 && chessBoard[i-4][j-4]==-1)
+                    if(i>=4 && j>=4 && chessBoard[i-1][j-1]==WHITE && chessBoard[i-2][j-2]==WHITE && chessBoard[i-3][j-3]==WHITE && chessBoard[i-4][j-4]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(i>=4 && j>=4 && chessBoard[i-1][j-1]==1 && chessBoard[i-2][j-2]==1 && chessBoard[i-3][j-3]==1 && chessBoard[i-4][j-4]==1)
+                    if(i>=4 && j>=4 && chessBoard[i-1][j-1]==BLACK && chessBoard[i-2][j-2]==BLACK && chessBoard[i-3][j-3]==BLACK && chessBoard[i-4][j-4]==BLACK)
                         valueTable[i][j] += 20000;
-                    if(i>=4 && j<=6 && chessBoard[i-1][j+1]==-1 && chessBoard[i-2][j+2]==-1 && chessBoard[i-3][j+3]==-1 && chessBoard[i-4][j+4]==-1)
+                    if(i>=4 && j<=6 && chessBoard[i-1][j+1]==WHITE && chessBoard[i-2][j+2]==WHITE && chessBoard[i-3][j+3]==WHITE && chessBoard[i-4][j+4]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(i>=4 && j<=6 && chessBoard[i-1][j+1]==1 && chessBoard[i-2][j+2]==1 && chessBoard[i-3][j+3]==1 && chessBoard[i-4][j+4]==1)
+                    if(i>=4 && j<=6 && chessBoard[i-1][j+1]==BLACK && chessBoard[i-2][j+2]==BLACK && chessBoard[i-3][j+3]==BLACK && chessBoard[i-4][j+4]==BLACK)
                         valueTable[i][j] += 20000;
-                    if(i<=5 && j>=4 && chessBoard[i+1][j-1]==-1 && chessBoard[i+2][j-2]==-1 && chessBoard[i+3][j-3]==-1 && chessBoard[i+4][j-4]==-1)
+                    if(i<=5 && j>=4 && chessBoard[i+1][j-1]==WHITE && chessBoard[i+2][j-2]==WHITE && chessBoard[i+3][j-3]==WHITE && chessBoard[i+4][j-4]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(i<=5 && j>=4 && chessBoard[i+1][j-1]==1 && chessBoard[i+2][j-2]==1 && chessBoard[i+3][j-3]==1 && chessBoard[i+4][j-4]==1)
+                    if(i<=5 && j>=4 && chessBoard[i+1][j-1]==BLACK && chessBoard[i+2][j-2]==BLACK && chessBoard[i+3][j-3]==BLACK && chessBoard[i+4][j-4]==BLACK)
                         valueTable[i][j] += 20000;
-                    if(i<=5 && j<=5 && chessBoard[i+1][j+1]==-1 && chessBoard[i+2][j+2]==-1 && chessBoard[i+3][j+3]==-1 && chessBoard[i+4][j+4]==-1)
+                    if(i<=5 && j<=5 && chessBoard[i+1][j+1]==WHITE && chessBoard[i+2][j+2]==WHITE && chessBoard[i+3][j+3]==WHITE && chessBoard[i+4][j+4]==WHITE)
                         valueTable[i][j] += 40000;
-                    if(i<=5 && j<=5 && chessBoard[i+1][j+1]==1 && chessBoard[i+2][j+2]==1 && chessBoard[i+3][j+3]==1 && chessBoard[i+4][j+4]==1)
+                    if(i<=5 && j<=5 && chessBoard[i+1][j+1]==BLACK && chessBoard[i+2][j+2]==BLACK && chessBoard[i+3][j+3]==BLACK && chessBoard[i+4][j+4]==BLACK)
                         valueTable[i][j] += 20000;
 
                 }
